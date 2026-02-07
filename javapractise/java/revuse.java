3c94d47e912d7aaf1e0e57c9c875b166952d20ae
devesh <devesht373@gmail.com>
2026-02-08 01:09:09 +0530

    New java Program

---
+++ javapractise/java/revuse.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class revuse {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n,rev=0;
+        System.out.println("Enter the value of n :");
+        n=sc.nextInt();
+
+      while(n>0){ 
+                  
+                  rev=rev*10+n%10;
+                  n=n/10;
+                  }
+                  System.out.println(rev);
+        }
+    }
+
