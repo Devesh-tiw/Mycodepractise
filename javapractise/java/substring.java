52af35184635f77ccef7acb889c23337fab9c8c2
devesh <devesht373@gmail.com>
2026-02-10 19:21:43 +0530

    Add some things in the code

--- javapractise/java/substring.java
+++ javapractise/java/substring.java
@@ -2,8 +2,21 @@ import java.util.*;
 public class substring {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String name ="Devesh Tiwari is good ";
-        String part =name.substring(5,12);
-System.out.println(part);
+       char temp;
+        String name;
+        System.out.println("Enter the String ");
+        name =sc.nextLine();
+        char[]arr=name.toCharArray();
+     for(int i=0,j=name.length()-1;i<j;i++,j--){
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j]= temp;            
+        }
+        for(int i=0;i<name.length();i++){
+        System.out.print(arr[i]);
+        }
+        for (int i = name.length()-1; i > 0; i--) {
+            System.out.print(arr[i]);
+        }
     }
 }
