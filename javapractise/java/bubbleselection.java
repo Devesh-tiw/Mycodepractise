2f245a3cfb6720d7c166e0f88b132b46cab5dfa2
devesh <devesht373@gmail.com>
2026-02-13 01:33:11 +0530

    bubble sorting

---
+++ javapractise/java/bubbleselection.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class bubbleselection{
+    public static void printarr(int arr[]){
+        for(int j=0;j<arr.length;j++){
+            System.out.print(arr[j]+" ");
+        }
+        System.out.println();
+    }
+public static void main(String[] args) {
+    Scanner sc=new Scanner (System.in );
+    int arr[]=new int[20];
+    int j,n,temp;
+    System.out.println("Enter the value of n :");
+    n=sc.nextInt();
+    System.out.println("Enter the value of array:");
+    for(int i=0;i<n;i++){ 
+    arr[i]=sc.nextInt();}
+    for (j = 0; j < n  - 1; j++){
+        for (int i = 0; i < n -j- 1; i++) {
+            if (arr[i]>arr[i+1]){
+                temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+
+            }
+        }
+       
+    }
+    printarr(arr);
+}}
