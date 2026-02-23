018ffc455aed890b4e8276c76af8c7a41a90db4c
devesh <devesht373@gmail.com>
2026-02-23 23:47:59 +0530

    leetcode

--- javapractise/java/searching.java
+++ javapractise/java/searching.java
@@ -1,26 +1,25 @@
 
-import java.util.*;
 
 public class searching {
 
     public static void main(String args[]) {
-         Scanner sc = new Scanner(System.in);
-         int n, x;
-         System.out.println("Enter the value size of Array:");
-         n = sc.nextInt();
-         int array[] = new int[50];
-         System.out.println("Enter the value of Array:");
-         for (int i = 0; i < n; i++) {
-            array[i] = sc.nextInt();
-         }
-         System.out.println("Enter the searching value:");
-         x = sc.nextInt();
-         for (int i = 0; i < array.length; i++){ 
-            if (x == array[i]){ 
-                System.out.print("The number is in the array "+ (i+1));
+        int nums[]={1,3,5,6};
+        int digit = 0;
+        int target=5;
+        int n = nums.length;
+        int X = nums[n-1];
+      
+        for (int i = 0; i < n; i++) {
+            if (target <= nums[i]) {
+                digit=i;
                 break;
             }
+
         }
+        if (target > X) {
+            System.out.println(n);
+        }
+        System.out.println(digit);
         
     }
 }
