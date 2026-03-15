efa8dfdff2652d7d98094c051b9010866090dd2f
devesh <devesht373@gmail.com>
2026-03-15 11:37:31 +0530

    practise of dynamic arrays

---
+++ CP/bubbledynmaic.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int *arr,n,i,j;
+    printf("Enter the alue of n ");
+    scanf("%d",&n);
+    arr=(int*)malloc(n*sizeof(int));
+    if (arr==NULL){
+        printf("Memory allocation is failed ");
+        return 1;
+
+    }
+    printf("Enter the value of array ");
+    for(i=0;i<n-1;i++)
+        scanf("%d", arr + i);
+    
+    for(i=0;i<n;i++){
+        for(j=0;j<n-1;j++){
+            if(arr[i]>arr[i+1]){
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    printf("%d",arr[i]);
+    
+}
