71ab8aeba6c6db15a0eb6b1fa911daa946526748
devesh <devesht373@gmail.com>
2026-04-01 23:51:57 +0530

    this is my work

--- javapractise/java/Employee.java
+++ javapractise/java/Employee.java
@@ -4,5 +4,13 @@ int Basic_Salary;
 
 }
 public class  Employee extends Earning{
-
+int DA;
+int HRA;
+int Employee_detail(String n,int BS,int D,int H){
+    name =n;
+    Basic_Salary=BS;
+    DA=D;
+    HRA=H;
+return 0;
+}
 }
