44aefb7bf7fb350f0f7baf81afa5b02381f65c44
devesh <devesht373@gmail.com>
2026-04-01 23:54:37 +0530

    this is code

--- javapractise/java/Employee.java
+++ javapractise/java/Employee.java
@@ -1,16 +1,16 @@
 class Earning{
     String name;
 int Basic_Salary;
-
+int total_salary;
 }
 public class  Employee extends Earning{
 int DA;
 int HRA;
-int Employee_detail(String n,int BS,int D,int H){
+void Employee_detail(String n,int BS,int D,int H){
     name =n;
     Basic_Salary=BS;
     DA=D;
     HRA=H;
-return 0;
+   
 }
 }
