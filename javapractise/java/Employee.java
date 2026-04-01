e6c281b66052f7893631b8f9fc9a4742151333cc
devesh <devesht373@gmail.com>
2026-04-02 00:08:26 +0530

    this is inheritant

--- javapractise/java/Employee.java
+++ javapractise/java/Employee.java
@@ -1,17 +1,25 @@
 class Earning{
     String name;
-int Basic_Salary;
+int Basic_Salary=250000;
 int total_salary;
 }
 public class  Employee extends Earning{
 int DA;
 int HRA;
-void Employee_detail(String n,int BS,int D,int H){
+void Employee_detail(String n,int D,int H){
     name =n;
-    Basic_Salary=BS;
+   
     DA=D;
     HRA=H;
-    total_salary=BS+D+H;
-    System.out.println("EMPLOYEE: "+name+" Basic Salary: "+Basic_Salary+" DA: "+ DA +" HRA "+HRA+"Total");
+    total_salary=Basic_Salary+D+H;
+    System.out.println("EMPLOYEE: "+name+" Basic Salary: "+Basic_Salary+" DA: "+ DA +" HRA:"+HRA +" Total Salary:"+total_salary);
+}
+public static void main(String[]args){
+    Employee E1=new Employee();
+     Employee E2=new Employee();
+      Employee E3=new Employee();
+    E1.Employee_detail("Ramesh",25100,65890);
+    E2.Employee_detail("Suresh",25100,90000);
+    E3.Employee_detail("Langesh",25191,65890);
 }
 }
