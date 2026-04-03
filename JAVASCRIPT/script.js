4e1d792085b219f04349f0855da45f04daa1def5
devesh <devesht373@gmail.com>
2026-04-03 20:54:51 +0530

    this is my code

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,9 +1,20 @@
 
-let a=prompt("Enter the Number");
+/*let a=prompt("Enter the Number");
 
 if(a%5==0){
     console.log("the number is multiple of 5");
 }
 else{
     console.log("this is not multiple of 5");
+}*/
+
+let a=prompt("Enter the persentage you got:");
+if(a>90){
+    console.log("A grade");
+}
+else if(a<90 && a>50){
+    console.log("B grade");
+}
+else{
+    console.log("Fail");
 }
