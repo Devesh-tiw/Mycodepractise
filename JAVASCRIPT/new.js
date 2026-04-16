cb84e45913d52c93eb6915cdf8ff9ab1603c6c28
devesh <devesht373@gmail.com>
2026-04-16 23:49:08 +0530

    this is my work

--- JAVASCRIPT/new.js
+++ JAVASCRIPT/new.js
@@ -8,7 +8,27 @@
 // }
 
 // console.log(typeof SHraddhaProfile.Name);
-let arr=[1,2,3,4];
-arr.forEach(function square(val){
-    console.log(val*val);
-})
+// let arr=[1,2,3,4];
+// arr.forEach(function square(val){
+//     console.log(val*val*val);
+// })
+
+//map
+
+// let sir=[12,23,31];
+
+// let dear =sir.map((val)=>{
+//     return val*2;
+// });
+// console.log(dear);
+
+// let me = {
+//     naming:'devesh',
+//     id:25106,
+// }
+// for(let key in me){
+//     console.log(key,me);
+// }
+
+//DOM is started 
+alert('I am gini')
