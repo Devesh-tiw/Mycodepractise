747337686efefff4008ad5583e909f0c26a44df6
devesh <devesht373@gmail.com>
2026-04-16 21:10:02 +0530

    this is  commit

--- JAVASCRIPT/new.js
+++ JAVASCRIPT/new.js
@@ -1,9 +1,14 @@
-const SHraddhaProfile={
-    Name:"Shraddha",
-    post:150,
-    follower:41100000,
-    following:4,
-    link:"@ShraddhaKhapra",
-    About:"Enterprenour Ex-Microsoft,Ex-DRDO,Current Educator",
-}
-console.log(typeof SHraddhaProfile.Name);
+// const SHraddhaProfile={
+//     Name:"Shraddha",
+//     post:150,
+//     follower:41100000,
+//     following:4,
+//     link:"@ShraddhaKhapra",
+//     About:"Enterprenour Ex-Microsoft,Ex-DRDO,Current Educator",
+// }
+
+// console.log(typeof SHraddhaProfile.Name);
+let arr=[1,2,3,4];
+arr.forEach(function square(val){
+    console.log(val*val);
+})
