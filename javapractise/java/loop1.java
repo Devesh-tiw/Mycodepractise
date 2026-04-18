acfeffe851ae6750436e35d13b2bfc619c713aba
devesh <devesht373@gmail.com>
2026-04-18 22:31:02 +0530

    this is project

---
+++ javapractise/java/loop1.java
@@ -0,0 +1,9 @@
+public class loop1 {
+    public static void main(String[] args) {
+       int i=0;
+        do { 
+            System.out.println("hello");
+            i++;
+        } while (i<5);
+    }
+}
