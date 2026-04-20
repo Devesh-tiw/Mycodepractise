b8a51cabf193a719b91e25ad1e5740d1aa9ecd0c
devesh <devesht373@gmail.com>
2026-04-20 23:15:26 +0530

    this is workspace

---
+++ OOPS/Exception.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Exception{
+    public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the Value of a");
+int a=sc.nextInt();
+System.out.println("Enter the value of B");
+int b=sc.nextInt();
+int c=0;
+try {
+     c=a/b;
+} catch (ArithmeticException e) {
+    System.out.println("Exception Handled "+e);
+}
+System.out.println(c);
+    }
+    
+}
