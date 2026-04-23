95bf1722fb0cebb9eec734e2ffba8e3eafc3728f
devesh <devesht373@gmail.com>
2026-04-23 12:06:59 +0530

    multiple exception work is doen

---
+++ OOPS/exceptionmulti.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class exceptionmulti {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in array:");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];  
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        try {
+            arr[n + 1] = 6;   
+        } catch (ArrayIndexOutOfBoundsException e) {  
+            System.out.println("Array exception handled: " + e);
+        }
+
+        System.out.println("Array elements:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
