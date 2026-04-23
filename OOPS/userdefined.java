37ac15daca5a8753485072fb03ab1dd5fbb69e88
devesh <devesht373@gmail.com>
2026-04-23 20:38:00 +0530

    checking

--- OOPS/userdefined.java
+++ OOPS/userdefined.java
@@ -1,19 +1,7 @@
 public class userdefined extends Exception {
 
     public userdefined(String Inlegalmessage) {
-        super(message);
-public class demo{
-    public static void main(String[] args) {
-        try {
-            int age=15;
-            if(age<18){
-                System.out.println("Inlegalmessage");
-            }
-        } catch ( userdefined e) {
 
-        }
-    }
-}
     }
     
 }
