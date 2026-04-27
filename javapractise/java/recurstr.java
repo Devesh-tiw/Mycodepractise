03d1f981236821d1593c128f04099ef8631ba6c8
devesh <devesht373@gmail.com>
2026-04-27 22:53:23 +0530

    this is recursion code

---
+++ javapractise/java/recurstr.java
@@ -0,0 +1,22 @@
+public class recurstr{
+   void stringrecur(String str) {
+    char x = 'x';
+    String be = "";
+    String ba = "";
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str.charAt(i) == x) {
+            be = be + str.charAt(i);
+        } else {
+            ba = ba + str.charAt(i);
+        }
+    }
+
+    System.out.println(ba + be);
+
+}
+    public static void main(String[] args) {
+          recurstr obj = new recurstr();
+       obj.stringrecur("axbcxxd");
+    }
+}
