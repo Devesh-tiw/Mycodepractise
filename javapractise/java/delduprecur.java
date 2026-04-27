801821ea6f23809788f852b59a306fac1aaa8e44
devesh <devesht373@gmail.com>
2026-04-27 23:17:25 +0530

    this is a code

---
+++ javapractise/java/delduprecur.java
@@ -0,0 +1,23 @@
+public class delduprecur {
+    public static boolean[] map=new boolean[26];
+    void deldup(int idx,String str,String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currchar=str.charAt(idx);
+        if(map[currchar-'a']){
+            deldup(idx+1, str, newString);
+        }
+        else{
+            newString+=currchar;
+            map[currchar-'a']=true;
+            deldup(idx+1, str, newString);
+
+        }
+    }
+    public static void main(String[] args) {
+         delduprecur obj = new delduprecur();
+        obj.deldup(0, "abbsssseeesssssssdddscac", "");
+    }
+}
