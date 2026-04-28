0f54d6f4e9ab54dcd24760bd48c59c29a44c764f
devesh <devesht373@gmail.com>
2026-04-29 00:30:03 +0530

    this is somehting

---
+++ javapractise/java/duplicate.java
@@ -0,0 +1,22 @@
+public class duplicate {
+    public static void main(String[]args){
+     int []arr = {1,2,3,4};
+     int flag=0;
+     for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr.length;j++){
+            if(arr[i]==arr[j]&& i!=j){
+                flag=1;
+            }
+        }
+     }
+     if(flag==1){
+System.out.println("Duplicate is here");
+     }
+     else{
+        System.out.println("not here");
+     }
+
+     }
+
+    }
+
