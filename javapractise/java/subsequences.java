4821299135ac470ff463b5126c5a3094314d791f
devesh <devesht373@gmail.com>
2026-05-03 21:16:34 +0530

    this is something

--- javapractise/java/subsequences.java
+++ javapractise/java/subsequences.java
@@ -9,6 +9,7 @@ char currentchar=str.charAt(idx);
 sub(str, idx+1, newstr+currentchar);
 sub(str, idx+1, newstr);
     }
+    
     public static void main(String[]args){
 subsequences s=new subsequences();
 s.sub("abc", 0, "");
