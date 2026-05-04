5eb2e9a41c2348295bc10f185eb146248d58a584
devesh <devesht373@gmail.com>
2026-05-04 12:33:58 +0530

    this is somehting

---
+++ javapractise/java/triangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class triangle {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+System.out.println("Enter the size of triangle");
+int n=sc.nextInt();
+for(int i=0;i<n;i++){
+    for(int j=n;j>i;j--){
+System.out.print(" ");
+    }
+    for (int k=0;k<i;k++) {
+        System.out.print(" *");
+    }
+    System.out.println(" ");
+}
+    }}
+
