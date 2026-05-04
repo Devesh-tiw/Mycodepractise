84d2264d08027a0e8d3a5e013a981b63b1f78da5
devesh <devesht373@gmail.com>
2026-05-04 12:04:12 +0530

    this is code

---
+++ javapractise/java/subrev.java
@@ -0,0 +1,16 @@
+public class subrev {
+    public static void subsequences(String str,int idx,String newstr){
+        if(idx==str.length()){
+            System.out.println(newstr);
+            return;
+        }
+        char currchar=str.charAt(idx);
+        subsequences(str, idx+1, newstr+currchar);
+        subsequences(str, idx+1, newstr);
+
+    }
+    public static void main(String[]args){
+        subrev s=new subrev();
+        s.subsequences("abc", 0, "");
+    }
+}
