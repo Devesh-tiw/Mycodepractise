dbced1c25eb1b09605478680e2067b72178f45f2
Devesh <devesht373@gmail.com>
2026-06-08 08:56:28 +0530

    this is somehitng

---
+++ Questionjava/TC.java
@@ -0,0 +1,11 @@
+public class TC {
+    public static void main(String[] args) {
+
+        double celsius = 28.5;
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("Temperature in Celsius: " + celsius + "°C");
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit + "°F");
+    }
+}
